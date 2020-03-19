d04d3b50163fa9b75a3d6c28e4c3737e1de99d3c
Alexander Koltun <alexander.koltun@onespan.com>
2020-03-19 18:24:34 +0100

    Add solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,18 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._chunkPart = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const chunks = (this._chunkPart + chunk.toString()).split(os.EOL);
+    this._chunkPart = chunks.pop();
+    chunks.forEach(line => this.push(line));
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this._chunkPart);
   }
 }
 
