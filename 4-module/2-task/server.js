0b1ae8a751aa33dcf4588dbb4249b22c84ba47e5
Alexander Koltun <alexander.koltun@onespan.com>
2020-03-24 17:49:03 +0100

    Add solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,17 +1,68 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+const limit = 1024 * 1024;
+
+const ERROR_MESSAGES = {
+  201: 'Created',
+  400: 'Request error',
+  409: 'File already exists',
+  413: 'File size limit exceeded',
+  500: 'Server error'
+};
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  const endResponse = status => {
+    res.statusCode = status;
+    res.end(ERROR_MESSAGES[status]);
+  };
+
   switch (req.method) {
     case 'POST':
+      if (path.dirname(pathname) !== '.') {
+        endResponse(400);
+      } else {
+        const limitSizeStream = new LimitSizeStream({ limit });
+        const fileStream = fs.createWriteStream(filepath, { flags: 'ax' });
+        req.pipe(limitSizeStream).pipe(fileStream);
 
+        limitSizeStream.on('error', error => {
+          if (error.code === 'LIMIT_EXCEEDED') {
+            fs.unlink(filepath, error => {
+              if (error) endResponse(500);
+              else endResponse(413);
+            });
+          } else {
+            endResponse(500);
+          }
+        });
+
+        fileStream.on('error', error => {
+          if (error.code === 'EEXIST') {
+            endResponse(409);
+          } else {
+            endResponse(500);
+          }
+        });
+
+        fileStream.on('finish', () => {
+          endResponse(201);
+        });
+
+        req.on('aborted', () => {
+          fs.unlink(filepath, () => {
+            req.destroy();
+          });
+        });
+      }
       break;
 
     default:
