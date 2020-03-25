c83e06248f0f540539658e2d0ec2dd47ecd12c0e
Alexander Koltun <alexander.koltun@gmail.com>
2020-03-25 19:08:29 +0100

    Fix solution for 4-module 2-task so it works with Node versions less than 12.13
    When error happened,
    - old Nodes (versions up to 12.12) emit finish event and then error event;
    - new Nodes (starting from 12.13) emit only error event (no finish anymore).

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -12,7 +12,7 @@ const ERROR_MESSAGES = {
   400: 'Request error',
   409: 'File already exists',
   413: 'File size limit exceeded',
-  500: 'Server error'
+  500: 'Server error',
 };
 
 server.on('request', (req, res) => {
@@ -20,7 +20,7 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
-  const endResponse = status => {
+  const endResponse = (status) => {
     res.statusCode = status;
     res.end(ERROR_MESSAGES[status]);
   };
@@ -30,13 +30,13 @@ server.on('request', (req, res) => {
       if (path.dirname(pathname) !== '.') {
         endResponse(400);
       } else {
-        const limitSizeStream = new LimitSizeStream({ limit });
-        const fileStream = fs.createWriteStream(filepath, { flags: 'ax' });
+        const limitSizeStream = new LimitSizeStream({limit});
+        const fileStream = fs.createWriteStream(filepath, {flags: 'ax'});
         req.pipe(limitSizeStream).pipe(fileStream);
 
-        limitSizeStream.on('error', error => {
+        limitSizeStream.on('error', (error) => {
           if (error.code === 'LIMIT_EXCEEDED') {
-            fs.unlink(filepath, error => {
+            fs.unlink(filepath, (error) => {
               if (error) endResponse(500);
               else endResponse(413);
             });
@@ -45,7 +45,7 @@ server.on('request', (req, res) => {
           }
         });
 
-        fileStream.on('error', error => {
+        fileStream.on('error', (error) => {
           if (error.code === 'EEXIST') {
             endResponse(409);
           } else {
@@ -53,8 +53,12 @@ server.on('request', (req, res) => {
           }
         });
 
-        fileStream.on('finish', () => {
-          endResponse(201);
+        // We have to use close instead of finish because on Node 12.12-
+        // finish happens even in case of error, befor error event.
+        // Starting from Node 12.13 there is only error event (no finish)
+        // if error happened, therefore it is possible to use finish event
+        fileStream.on('close', () => {
+          if (fileStream.writableFinished) endResponse(201);
         });
 
         req.on('aborted', () => {
