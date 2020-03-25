bc0a97d8cfb15acdc361ae5439a8537ae3d8a32e
Alexander Koltun <alexander.koltun@gmail.com>
2020-03-25 22:38:15 +0100

    Add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
 
+let subscribers = [];
+
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
@@ -9,9 +11,19 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/subscribe', async (ctx, next) => {
+  ctx.body = await (new Promise((resolve) => subscribers.push(resolve)));
+  ctx.status = 200;
+  next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  if (message) {
+    subscribers.forEach((subscriber) => subscriber(message));
+    subscribers = [];
+    ctx.status = 200;
+  }
+  next();
 });
 
 app.use(router.routes());
