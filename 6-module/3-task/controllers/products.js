16bf923cda034543f779cf727519fe9d295ab196
Alexander Koltun <alexander.koltun@onespan.com>
2020-04-06 18:32:05 +0200

    Add solution 6-module 3-task

--- 6-module/3-task/controllers/products.js
+++ 6-module/3-task/controllers/products.js
@@ -1,3 +1,20 @@
+const Product = require('../models/Product');
+
+const transform = ({
+  _id: id,
+  title,
+  images,
+  category,
+  subcategory,
+  price,
+  description
+}) => ({ id, title, images, category, subcategory, price, description });
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const { query } = ctx.query;
+  const products = query
+    ? (await Product.find({ $text: { $search: query } })).map(transform)
+    : [];
+
+  ctx.body = { products };
 };
